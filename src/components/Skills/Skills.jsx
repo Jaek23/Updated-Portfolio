af90f07f8b8a0b98e44c68804deaf38746441bce
jae kim <jaehkim23@gmail.com>
2024-02-29 18:11:59 -0600

    finished skills section of portfolio

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,16 +6,16 @@ import styles from "./Skills.module.css";
 const Skills = () => {
   return (
     <section id='skills' className={styles.container}>
-       <h2>Skills</h2>
-        <div>
+       <h2 className={styles.title}>Skills</h2>
+        <div className={styles.skills}>
             {skills.map((skill, id) => {
               return(
-                <>
-                    <div key={id}> 
+                <div key={id} className={styles.skill}>
+                    <div className={styles.skillImageContainer}> 
                         <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
-                        <p>{skill.title}</p>
                     </div>
-                </>
+                    <p>{skill.title}</p>
+                </div>
               )
             })}
        </div>
