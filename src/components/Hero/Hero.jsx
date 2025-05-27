262c04bc67445cdaebf73a688c4eb5e6f67e6483
jae kim <jaehkim23@gmail.com>
2025-05-27 16:15:16 -0500

    updated projects

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,16 @@ const Hero = () => {
     <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Jae</h1>
-            <p className={styles.description}>Aspiring software engineer with a background in marketing and sales. I completed the Full Stack Web Development Bootcamp at the University of Texas at Austin in 2023. I enjoy problem solving and creating user-friendly and visually appealing applications. I am continuously learning new skills to advance my full-stack developing skills. My skills include HTML, CSS, JavaScript, React, Express, Node, and Git.</p>
+            <p className={styles.description}>
+              Full stack Developer with over a year of professional experience and a background in marketing and sales.
+              A graduate of the Full stack Web Development Bootcamp at the Univeristy of Texas at Austin, I specialize in 
+              building user-friendly, visually engaging web and mobile applications. I'm passionate about solving problems, writing 
+              clean and efficient code, and continuously expanding my skill set. My technical toolkit includes HTML, CSS,
+              JavaScript, TypeScript, React, Next.js, Node.js, Express, PostgresSQL, and Git. I'm also deeply interested in 
+              development workflows and quality assurance, with a strong focus on building reliable and maintainable 
+              software. 
+
+            </p>
             <a href="https://docs.google.com/document/d/17wcc7avjUgzA59OJ9VIEKJ4Don3TxAvFJJZCxvOGVMw/edit" className={styles.contactBtn} target='null'>View CV</a>
         </div>
         <img src={getImageUrl("hero/jae.PNG")} alt="image of me" className={styles.heroImg}/>
